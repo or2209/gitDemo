ca33e39b7eddbc3a10d1f0823a92e7ebc430566a
or2209 <orlaharty1@gmail.com>
2024-05-01 12:55:29 +0300

    back to root

--- src/main.java
+++ src/main.java
@@ -2,7 +2,6 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("hello world from b2 considering branch 1");
-        System.out.println("bad commit");
-        System.out.println("bad commit");
+
     }
 }
